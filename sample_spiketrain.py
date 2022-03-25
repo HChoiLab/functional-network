4ba5a2616355815ec3f90616b2244cd9c3fe781c
Disheng Tang <72563966+DishengTang@users.noreply.github.com>
2022-03-25 16:08:20 +0800

    Rewrote all functions

--- sample_spiketrain.py
+++ sample_spiketrain.py
@@ -1,119 +1,77 @@
 #%%
+from lib2to3.pgen2.grammar import Grammar
 import numpy as np
 import matplotlib.pyplot as plt
 
 from ClosedSynchrony import *
-from PatternJitter import *
+# from PatternJitter import *
 from TransitMatrix import *
 from Data import *
 from tqdm import tqdm
 
-# x_tilde = [10, 15, 22, 29, 34, 40, 45, 51]
-# L = 5
-# R = 4
-Ref = [8, 13, 19, 28, 34, 42, 44, 49]
-Ref_02 = [10, 14, 17]
-Ref_03 = [10, 14, 17, 20]
+def getSpikeTrain(spikeData):
+    spikeTrain = np.squeeze(np.where(spikeData>0))
+    return spikeTrain
 
-
-def getX1(Dist, L, R, ObsX):
-     # in [0,1)
-    # print('Transition Dist.:', dist)
-    # print('Random X:', randX)
-    up = 0
-    sum = 0
-    length = 0
-    hisLen = 0
-
-    dist = []
-    obsTar = []
-
-    length = L
-    hisLen = R
-    dist = Dist
-    obsTar = ObsX
-
-    # Omega = getOmega(length, obsTar)
-    Omega = getGamma(R, L, obsTar)
-    result = Omega[0][up]
+def getX1(initDist, L, R, spikeTrain):
+    # Omega = getOmega(L, obsTar)
+    Gamma = getGamma(L, R, spikeTrain)
     randX = np.random.random()
-
-    for k in dist:
-        # print('P[',i,']:', k)
-        sum += k
-        if randX <= sum:
-            # print('Sum:', sum)
-            return result
-        up += 1
-        result = Omega[0][up]
+    ind = np.where(randX <= np.cumsum(initDist))[0][0]
+    return Gamma[0][ind]
 
 def initializeX(initX, Prob):
+    return initX + np.sum(Prob == 0)
 
-    init_x = initX
-    prob = Prob
-    m = len(prob)
-    for k in range(m):
-        if prob[k] == 0:
-            init_x += 1
+def getGamma(L, R, spikeTrain):
 
-    return init_x
+    Gamma = []
+    ks = [] # list of k_d
+    ks.append(0)
+    n = len(spikeTrain)
+    temp = int(spikeTrain[ks[-1]]/L)*L
+    Gamma.append(np.arange(temp, temp + L, 1))
+    for i in range(1, n):
+        if spikeTrain[i] - spikeTrain[i-1] > R:
+            ks.append(i)
+        temp = int(spikeTrain[ks[-1]]/L)*L+spikeTrain[i]-spikeTrain[ks[-1]]
+        Gamma.append(np.arange(temp, temp + L, 1))
+    return Gamma
 
-# getSpikeTrain(ObsTar, length, hisLen, initD, tDistMat)
+# getSpikeTrain(ObsTar, L, R, initD, tDistMat)
 
 def getSurrogate(spikeTrain, L, R, initDist, tDistMatrices):
 
-    # print('////**** Simulation is starting. ****////')
-
     chain = 1
     surrogate = []
-
-    length = L
-    hisLen = R
-    # Omega = getOmega(length, spikeTrain)
-    Omega = getGamma(R, length, spikeTrain)
-    ks = [] # list of kd
-    ks.append(0)
-    x1 = getX1(initDist, length, hisLen, spikeTrain)
-    givenX = x1
-    surrogate.append(x1)
-
+    # Omega = getOmega(L, spikeTrain)
+    Gamma = getGamma(L, R, spikeTrain)
+    givenX = getX1(initDist, L, R, spikeTrain)
+    surrogate.append(givenX)
     for i, row in enumerate(tDistMatrices):
         if spikeTrain[i+1] - spikeTrain[i] <= R:
-            X = surrogate[-1] + spikeTrain[i+1] - spikeTrain[i]
-            surrogate.append(X)
-            givenX = X
-            chain += 1
+            givenX = surrogate[-1] + spikeTrain[i+1] - spikeTrain[i]
         else:
-            sum = 0
-            randX = 0
-            index = np.where(np.array(Omega[i]) == givenX)[0]
+            index = np.where(np.array(Gamma[i]) == givenX)[0]
             p_i = np.squeeze(np.array(row[index]))
-            initX = initializeX(Omega[chain][0], p_i)
+            initX = initializeX(Gamma[chain][0], p_i)
             randX = np.random.random()
-            m = len(p_i)
-            for j in range(m):
-                if p_i[j] != 0:
-                    sum += p_i[j]
-                    if randX <= sum:
-                        surrogate.append(initX)
-                        givenX = initX
-                        chain += 1
-                        break
-                    initX += 1
-                else:
-                    j += 1
-        
-    # print('////**** Simulation is done. ****////', '\n')
+            # safe way to find the ind
+            larger = np.where(randX <= np.cumsum(p_i))[0]
+            if larger.shape[0]:
+                ind = larger[0]
+            else:
+                ind = len(p_i) - 1
+            givenX = initX + np.sum(p_i[:ind]!=0)
+        surrogate.append(givenX)
+        chain += 1
     return surrogate
 
 def getSpikeTrainMat(L, R, spikeTrain, initDist, tDistMatrices, N):
 
-    length = L
-    hisLen = R
     spikeTrainMat = []
     for i in tqdm(range(N)):
-        # print('[[[[[[[Spike Train Index: ', i,']]]]]]]')
-        surrogate = getSurrogate(spikeTrain, length, hisLen, initDist, tDistMatrices)
+        surrogate = getSurrogate(spikeTrain, L, R, initDist, tDistMatrices)
         spikeTrainMat.append(surrogate)
     Tmat = np.array(spikeTrainMat)
     return Tmat
@@ -136,7 +94,7 @@ def getAmountSync(Reference, Target):
 
 L = 50
 R = 10
-fRate = 20#40
+fRate = 10#40
 Size = 1000#100
 spikeData = getSpikeData(Size, fRate)
 spikeTrain = getSpikeTrain(spikeData)
@@ -170,8 +128,8 @@ fig = plt.figure(figsize=(6, 4))
 plt.eventplot(np.concatenate((spikeTrain[None, :], Tmat[:num, :]), axis=0), colors=colors, lineoffsets=1, linewidths=1, linelengths=1)
 plt.axis('off')
 plt.gca().invert_yaxis()
-Omega = getGamma(R, L, spikeTrain)
-# plt.vlines(np.concatenate((np.min(Omega, axis=1), np.max(Omega, axis=1))), ymin=0, ymax=num+1, colors='k', linewidth=0.2, linestyles='dashed')
+Gamma = getGamma(L, R, spikeTrain)
+# plt.vlines(np.concatenate((np.min(Gamma, axis=1), np.max(Gamma, axis=1))), ymin=0, ymax=num+1, colors='k', linewidth=0.2, linestyles='dashed')
 plt.tight_layout()
 plt.show()
 # plt.savefig('./plots/raster.jpg')
@@ -201,9 +159,54 @@ for ind, t_pos in enumerate(text_pos):
   plt.text(-80, t_pos, 'R={}'.format(R_list[ind]), size=10, color=Palette[ind], weight='bold')
 plt.axis('off')
 plt.gca().invert_yaxis()
-Omega = getGamma(R, L, spikeTrain)
-# plt.vlines(np.concatenate((np.min(Omega, axis=1), np.max(Omega, axis=1))), ymin=0, ymax=num+1, colors='k', linewidth=0.2, linestyles='dashed')
+Gamma = getGamma(L, R, spikeTrain)
+# plt.vlines(np.concatenate((np.min(Gamma, axis=1), np.max(Gamma, axis=1))), ymin=0, ymax=num+1, colors='k', linewidth=0.2, linestyles='dashed')
 plt.tight_layout()
 plt.savefig('../plots/sampled_spiketrain_L{}.jpg'.format(L))
 # plt.show()
+# %%
+def load_npz(filename):
+    """
+    load npz files with sparse matrix and dimension
+    output dense matrix with the correct dim
+    """
+    npzfile = np.load(filename, allow_pickle=True) 
+    sparse_matrix = npzfile['arr_0'][0]
+    ndim=npzfile['arr_0'][1]
+
+    new_matrix_2d = np.array(sparse_matrix.todense())
+    new_matrix = new_matrix_2d
+    # new_matrix = new_matrix_2d.reshape(ndim)
+    return new_matrix
+import os
+import time
+min_len, min_num = (10000, 29)
+min_spikes = min_len * 0.002 # 2 Hz
+# measure = 'pearson'
+# measure = 'cosine'
+# measure = 'correlation'
+# measure = 'MI'
+measure = 'xcorr'
+# measure = 'causality'
+directory = '../data/ecephys_cache_dir/sessions/spiking_sequence/'
+files = os.listdir(directory)
+files.sort(key=lambda x:int(x[:9]))
+path = os.path.join(directory.replace('spiking_sequence', 'adj_mat_{}'.format(measure)))
+if not os.path.exists(path):
+  os.makedirs(path)
+num_sample = 20
+portions = np.arange(0.05, 1.05, 0.05)
+# %%
+start_time = time.time()
+for file in files:
+  if file.endswith(".npz"):
+    start_time_mouse = time.time()
+    print(file)
+    mouseID = file.replace('.npz', '').split('_')[0]
+    stimulus = file.replace('.npz', '').replace(mouseID + '_', '')
+    break
+sequences = load_npz(os.path.join(directory, file))
+sequences = sequences[np.count_nonzero(sequences[:, :min_len], axis=1) > 80, :min_len]
+num_nodes = sequences.shape[0]
+
 # %%
