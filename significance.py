c32ff2a6158893688be0feb23e910bef997ca7a8
Disheng Tang <72563966+DishengTang@users.noreply.github.com>
2022-07-19 17:38:49 +0800

    Add check double count 0 time lag edges

--- significance.py
+++ significance.py
@@ -1,4 +1,5 @@
 #%%
+from sys import breakpointhook
 from library import *
 #%%
 start_time = time.time()
@@ -21,3 +22,85 @@ save_active_inds(min_FR, session_ids, stimulus_names)
 area_dict = load_area_dict(session_ids)
 save_active_area_dict(area_dict)
 # %%
+########### check double count 0 time lag edges
+directory = './data/ecephys_cache_dir/sessions/adj_mat_ccg_highland_corrected/'
+files = os.listdir(directory)
+files.sort(key=lambda x:int(x[:9]))
+for file in files:
+  if file.endswith(".npz") and ('gabors' not in file) and ('_offset' not in file) and ('_duration' not in file) and ('_bl' not in file) and ('confidence' not in file):
+    print(file)
+    # break
+    adj_mat = load_npz_3d(os.path.join(directory, file))
+    offset_mat = load_npz_3d(os.path.join(directory, file.replace('.npz', '_offset.npz')))
+    duration_mat = load_npz_3d(os.path.join(directory, file.replace('.npz', '_duration.npz')))
+    confidence_level = load_npz_3d(os.path.join(directory, file.replace('.npz', '_confidence.npz')))
+    print('{}, number of double count 0 edges is {}'.format(file, ((offset_mat==0) & (offset_mat.T==0)).sum()))
+# %%
+start_time = time.time()
+measure = 'ccg'
+min_spike = 50
+n = 4
+max_duration = 12
+directory = './data/ecephys_cache_dir/sessions/adj_mat_{}_corrected/'.format(measure)
+save_ccg_corrected_highland(directory, measure, min_spike=min_spike, max_duration=max_duration, maxlag=12, n=n)
+print("--- %s minutes in total" % ((time.time() - start_time)/60))
+#%%
+G_ccg_dict, offset_dict, duration_dict = load_highland_xcorr(path, active_area_dict, weight=True)
+########### plot time offset distribution of reciprocal edge
+def reciprocal_offset_heatmap(offset_dict, duration_dict, measure, n):
+  rows, cols = get_rowcol(G_dict)
+  scale = np.zeros(len(rows))
+  region_connection = np.zeros((len(rows), len(cols), len(regions), len(regions)))
+  for row_ind, row in enumerate(rows):
+    print(row)
+    for col_ind, col in enumerate(cols):
+      G = G_dict[row][col] if col in G_dict[row] else nx.Graph()
+      if G.number_of_nodes() >= 2 and G.number_of_edges() > 0:
+        nodes = list(G.nodes())
+        active_areas = np.unique(list({key: area_dict[row][key] for key in nodes}.values()))
+        # active_areas = [i for i in regions if i in active_areas]
+        A = nx.adjacency_matrix(G)
+        A = A.todense()
+        A[A.nonzero()] = 1
+        for region_ind_i, region_i in enumerate(regions):
+          if region_i in active_areas:
+            for region_ind_j, region_j in enumerate(regions):
+              if region_j in active_areas:
+                region_indices_i = np.array([k for k, v in area_dict[row].items() if v==region_i])
+                region_indices_j = np.array([k for k, v in area_dict[row].items() if v==region_j])
+                region_indices_i = np.array([nodes.index(i) for i in list(set(region_indices_i) & set(nodes))]) # some nodes not in cc are removed 
+                region_indices_j = np.array([nodes.index(i) for i in list(set(region_indices_j) & set(nodes))])
+                region_connection[row_ind, col_ind, region_ind_i, region_ind_j] = np.sum(A[region_indices_i[:, None], region_indices_j])
+                assert np.sum(A[region_indices_i[:, None], region_indices_j]) == len(A[region_indices_i[:, None], region_indices_j].nonzero()[0])
+      region_connection[row_ind, col_ind, :, :] = region_connection[row_ind, col_ind, :, :] / region_connection[row_ind, col_ind, :, :].sum()
+    scale[row_ind] = region_connection[row_ind, :, :, :].max()
+  ind = 1
+  fig = plt.figure(figsize=(4*len(cols), 3*len(rows)))
+  left, width = .25, .5
+  bottom, height = .25, .5
+  right = left + width
+  top = bottom + height
+  for row_ind, row in enumerate(rows):
+    print(row)
+    for col_ind, col in enumerate(cols):
+      plt.subplot(len(rows), len(cols), ind)
+      if row_ind == 0:
+        plt.gca().set_title(cols[col_ind], fontsize=20, rotation=0)
+      if col_ind == 0:
+        plt.gca().text(0, 0.5 * (bottom + top), rows[row_ind],
+        horizontalalignment='left',
+        verticalalignment='center',
+        # rotation='vertical',
+        transform=plt.gca().transAxes, fontsize=20, rotation=90)
+      plt.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+      ind += 1
+      sns_plot = sns.heatmap(region_connection[row_ind, col_ind, :, :].astype(float), vmin=0, vmax=scale[row_ind],center=0,cmap="RdBu_r")# cmap="YlGnBu"
+      # sns_plot = sns.heatmap(region_connection.astype(float), vmin=0, cmap="YlGnBu")
+      sns_plot.set_xticks(np.arange(len(regions))+0.5)
+      sns_plot.set_xticklabels(regions, rotation=90)
+      sns_plot.set_yticks(np.arange(len(regions))+0.5)
+      sns_plot.set_yticklabels(regions, rotation=0)
+      sns_plot.invert_yaxis()
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('./plots/reciprocal_offset_{}_{}fold.jpg'.format(measure, n))
