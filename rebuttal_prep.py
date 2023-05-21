5670d3cbb1d2bc988c7d29ea48398956a9f8a703
Disheng Tang <72563966+DishengTang@users.noreply.github.com>
2023-05-22 00:14:27 +0800

    Create rebuttal_prep.py

---
+++ rebuttal_prep.py
@@ -0,0 +1,111 @@
+#%%
+from library import *
+combined_stimuli = [['spontaneous'], ['flash_dark', 'flash_light'], ['drifting_gratings'], ['static_gratings'], ['natural_scenes'], ['natural_movie_one', 'natural_movie_three']]
+combined_stimulus_names = ['Resting\nstate', 'Flashes', 'Drifting\ngratings', 'Static\ngratings', 'Natural\nscenes', 'Natural\nmovies']
+combined_stimulus_colors = ['#8dd3c7', '#fee391', '#bc80bd', '#bc80bd', '#fb8072', '#fb8072']
+# plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
+session2keep = ['719161530','750749662','754312389','755434585','756029989','791319847','797828357']
+stimulus_by_type = [['spontaneous'], ['flash_dark', 'flash_light'], ['drifting_gratings', 'static_gratings'], ['natural_scenes', 'natural_movie_one', 'natural_movie_three']]
+stimulus_types = ['Resting state', 'Flashes', 'Gratings', 'Natural stimuli']
+# stimulus_type_color = ['tab:blue', 'darkorange', 'darkgreen', 'maroon']
+stimulus_type_color = ['#8dd3c7', '#fee391', '#bc80bd', '#fb8072']
+stimulus_labels = ['Resting\nstate', 'Dark\nflash', 'Light\nflash', 'Drifting\ngrating', 
+              'Static\ngrating', 'Natural\nscenes', 'Natural\nmovie 1', 'Natural\nmovie 3']
+region_labels = ['AM', 'PM', 'AL', 'RL', 'LM', 'V1']
+# region_colors = ['#b3de69', '#80b1d3', '#fdb462', '#d9d9d9', '#fccde5', '#bebada']
+region_colors = ['#d9e9b5', '#c0d8e9', '#fed3a1', '#c3c3c3', '#fad3e4', '#cec5f2']
+TRIAD_NAMES = ('003', '012', '102', '021D', '021U', '021C', '111D', '111U', '030T', '030C', '201', '120D', '120U', '120C', '210', '300')
+model_names = [u'Erdős-Rényi model', 'Degree-preserving model', 'Pair-preserving model', 'Signed-pair-preserving model']
+
+area_dict, active_area_dict, mean_speed_df = load_other_data(session_ids)
+cortical_inds = get_cortical_inds(active_area_dict, visual_regions)
+directory = './data/ecephys_cache_dir/sessions/spiking_sequence/'
+path = directory.replace('spiking_sequence', 'adj_mat_ccg_highland_corrected')
+if not os.path.exists(path):
+  os.makedirs(path)
+G_ccg_dict, offset_dict, duration_dict = load_highland_xcorr(path, active_area_dict, weight=True)
+measure = 'ccg'
+G_ccg_dict = remove_gabor(G_ccg_dict)
+######### removed neurons from thalamic region
+G_ccg_dict = remove_thalamic(G_ccg_dict, area_dict, visual_regions)
+offset_dict = remove_thalamic_mat(offset_dict, active_area_dict, visual_regions)
+duration_dict = remove_thalamic_mat(duration_dict, active_area_dict, visual_regions)
+active_area_dict = remove_thalamic_area(active_area_dict, visual_regions)
+n = 4
+S_ccg_dict = add_sign(G_ccg_dict)
+S_ccg_dict = add_offset(S_ccg_dict, offset_dict)
+S_ccg_dict = add_duration(S_ccg_dict, duration_dict)
+S_ccg_dict = add_delay(S_ccg_dict)
+######### split G_dict into pos and neg
+pos_G_dict, neg_G_dict = split_pos_neg(G_ccg_dict)
+# %%
+old_combined_stimuli = [['spontaneous'], ['flashes'], ['drifting_gratings'], ['static_gratings'], ['natural_scenes'], ['natural_movie_one', 'natural_movie_three']]
+combined_mspeed_df = pd.DataFrame()
+for cs_ind, combined_stimulus in enumerate(old_combined_stimuli):
+  combined_mspeed_df[combined_stimulus_names[cs_ind]] = mean_speed_df[combined_stimulus].mean(axis=1)
+# combined_mspeed_df
+# %%
+# %%
+seven_session_ids = ['719161530', '750749662', '754312389', '755434585', '756029989', '791319847', '797828357']
+color_list = [u'#1f77b4', u'#ff7f0e', u'#2ca02c', u'#d62728', u'#9467bd', u'#8c564b', u'#e377c2', u'#7f7f7f', u'#bcbd22', u'#17becf']
+mean_speed = combined_mspeed_df.loc[seven_session_ids].values.astype(float)
+fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+for s_ind, session_id in enumerate(seven_session_ids):
+  ax.plot(range(6), mean_speed[s_ind], color=color_list[s_ind], label=session_id)
+ax.legend()
+ax.set_xticks(range(6))
+ax.set_xticklabels(combined_stimulus_names, fontsize=12)
+# ax.set_yscale('log')
+ax.set_ylabel('mean speed', fontsize=20)
+plt.savefig('./plots/mean_speed_eachmouse.pdf', transparent=True)
+# %%
+# TODO: separate session into still and running
+def load_area_speed(session_ids, stimulus_names, regions):
+  data_directory = './data/ecephys_cache_dir'
+  manifest_path = os.path.join(data_directory, "manifest.json")
+  cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
+  area_dict = {}
+  speed_dict = {}
+  for mouseID in session_ids:
+    print(mouseID)
+    session = cache.get_session_data(int(mouseID),
+                                amplitude_cutoff_maximum=np.inf,
+                                presence_ratio_minimum=-np.inf,
+                                isi_violations_maximum=np.inf)
+    df = session.units
+    df = df.rename(columns={"channel_local_index": "channel_id", 
+                            "ecephys_structure_acronym": "ccf", 
+                            "probe_id":"probe_global_id", 
+                            "probe_description":"probe_id",
+                            'probe_vertical_position': "ypos"})
+    cortical_units_ids = np.array([idx for idx, ccf in enumerate(df.ccf.values) if ccf in regions])
+    df_cortex = df.iloc[cortical_units_ids]
+    instruction = df_cortex.ccf
+    # if set(instruction.unique()) == set(regions): # if the mouse has all regions recorded
+    #   speed_dict[mouseID] = {}
+    instruction = instruction.reset_index()
+    if not mouseID in area_dict:
+      area_dict[mouseID] = {}
+      speed_dict[mouseID] = {}
+    for i in range(instruction.shape[0]):
+      area_dict[mouseID][i] = instruction.ccf.iloc[i]
+    for stimulus_name in stimulus_names:
+      print(stimulus_name)
+      stim_table = session.get_stimulus_table([stimulus_name])
+      stim_table=stim_table.rename(columns={"start_time": "Start", "stop_time": "End"})
+      if 'natural_movie' in stimulus_name:
+        frame_times = stim_table.End-stim_table.Start
+        print('frame rate:', 1/np.mean(frame_times), 'Hz', np.mean(frame_times))
+        # stim_table.to_csv(output_path+'stim_table_'+stimulus_name+'.csv')
+        # chunch each movie clip
+        stim_table = stim_table[stim_table.frame==0]
+      speed = session.running_speed[(session.running_speed['start_time']>=stim_table['Start'].min()) & (session.running_speed['end_time']<=stim_table['End'].max())]
+      speed_dict[mouseID][stimulus_name] = speed['velocity'].mean()
+  # switch speed_dict to dataframe
+  mouseIDs = list(speed_dict.keys())
+  stimuli = list(speed_dict[list(speed_dict.keys())[0]].keys())
+  mean_speed_df = pd.DataFrame(columns=stimuli, index=mouseIDs)
+  for k in speed_dict:
+    for v in speed_dict[k]:
+      mean_speed_df.loc[k][v] = speed_dict[k][v]
+  return area_dict, mean_speed_df
