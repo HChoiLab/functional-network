94d7044390e006ee463e0f520b63a67359bdc8ca
Disheng Tang <72563966+DishengTang@users.noreply.github.com>
2023-05-31 23:25:53 +0800

    Add connection/motif comparison

--- rebuttal_prep.py
+++ rebuttal_prep.py
@@ -345,81 +345,221 @@ fname = os.path.join(path, file)
 start_time = time.time()
 save_mean_ccg_corrected_stationary_running(o_sequences=sequences, inds=run_ind, fname=fname, num_jitter=num_baseline, L=25, window=100, disable=False)
 print("--- %s minutes" % ((time.time() - start_time)/60))
-#%%
-mouseID = '719161530'
-stimulus_name = 'drifting_gratings'
-data_directory = './data/ecephys_cache_dir'
-manifest_path = os.path.join(data_directory, "manifest.json")
-cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
-session = cache.get_session_data(int(mouseID),
-                                amplitude_cutoff_maximum=np.inf,
-                                presence_ratio_minimum=-np.inf,
-                                isi_violations_maximum=np.inf)
-stim_table = session.get_stimulus_table([stimulus_name])
-stim_table=stim_table.rename(columns={"start_time": "Start", "stop_time": "End"})
-# block_ids = stim_table['stimulus_block'].unique()
-all_speed = []
-for i in stim_table.index: # each row is a presentation
-  speed = session.running_speed[(session.running_speed['start_time']>=stim_table.loc[i, 'Start']) & (session.running_speed['end_time']<=stim_table.loc[i, 'End'])]
-  all_speed.append(speed.velocity.values)
-min_len = min(len(lst) for lst in all_speed)
-# Create the 2D NumPy array
-speed_trial = np.array([speed[:min_len] for speed in all_speed])
-plt.figure()
-m, std = np.nanmean(speed_trial, 0), np.nanstd(speed_trial, 0)
-plt.plot(range(speed_trial.shape[1]), m, color='blue')
-plt.fill_between(range(speed_trial.shape[1]), m-std,m+std, color='blue', alpha=0.2)
-plt.xlabel('time bin since onset')
-plt.ylabel('running speed (cm/s)')
-plt.show()
 # %%
-# TODO: separate session into still and running
-def load_area_speed(session_ids, stimulus_names, regions):
-  data_directory = './data/ecephys_cache_dir'
-  manifest_path = os.path.join(data_directory, "manifest.json")
-  cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
-  area_dict = {}
-  speed_dict = {}
-  for mouseID in session_ids:
-    print(mouseID)
-    session = cache.get_session_data(int(mouseID),
-                                amplitude_cutoff_maximum=np.inf,
-                                presence_ratio_minimum=-np.inf,
-                                isi_violations_maximum=np.inf)
-    df = session.units
-    df = df.rename(columns={"channel_local_index": "channel_id", 
-                            "ecephys_structure_acronym": "ccf", 
-                            "probe_id":"probe_global_id", 
-                            "probe_description":"probe_id",
-                            'probe_vertical_position': "ypos"})
-    cortical_units_ids = np.array([idx for idx, ccf in enumerate(df.ccf.values) if ccf in regions])
-    df_cortex = df.iloc[cortical_units_ids]
-    instruction = df_cortex.ccf
-    # if set(instruction.unique()) == set(regions): # if the mouse has all regions recorded
-    #   speed_dict[mouseID] = {}
-    instruction = instruction.reset_index()
-    if not mouseID in area_dict:
-      area_dict[mouseID] = {}
-      speed_dict[mouseID] = {}
-    for i in range(instruction.shape[0]):
-      area_dict[mouseID][i] = instruction.ccf.iloc[i]
-    for stimulus_name in stimulus_names:
-      print(stimulus_name)
-      stim_table = session.get_stimulus_table([stimulus_name])
-      stim_table=stim_table.rename(columns={"start_time": "Start", "stop_time": "End"})
-      if 'natural_movie' in stimulus_name:
-        frame_times = stim_table.End-stim_table.Start
-        print('frame rate:', 1/np.mean(frame_times), 'Hz', np.mean(frame_times))
-        # stim_table.to_csv(output_path+'stim_table_'+stimulus_name+'.csv')
-        # chunch each movie clip
-        stim_table = stim_table[stim_table.frame==0]
-      speed = session.running_speed[(session.running_speed['start_time']>=stim_table['Start'].min()) & (session.running_speed['end_time']<=stim_table['End'].max())]
-      speed_dict[mouseID][stimulus_name] = speed['velocity'].mean()
-  # switch speed_dict to dataframe
-  mouseIDs = list(speed_dict.keys())
-  stimuli = list(speed_dict[list(speed_dict.keys())[0]].keys())
-  mean_speed_df = pd.DataFrame(columns=stimuli, index=mouseIDs)
-  for k in speed_dict:
-    for v in speed_dict[k]:
-      mean_speed_df.loc[k][v] = speed_dict[k][v]
-  return area_dict, mean_speed_df
+# save significant CCG for stationary and running trials
+start_time = time.time()
+measure = 'ccg'
+min_spike = 50
+n = 4
+max_duration = 11
+maxlag = 12
+directory = './data/ecephys_cache_dir/sessions/adj_mat_{}_corrected_stationary/'.format(measure)
+save_ccg_corrected_highland_new(directory, measure, min_spike=min_spike, max_duration=max_duration, maxlag=maxlag, n=n)
+directory = './data/ecephys_cache_dir/sessions/adj_mat_{}_corrected_running/'.format(measure)
+save_ccg_corrected_highland_new(directory, measure, min_spike=min_spike, max_duration=max_duration, maxlag=maxlag, n=n)
+print("--- %s minutes in total" % ((time.time() - start_time)/60))
+# %%
+##################### keep edges above threshold
+threshold = 0.9
+directory = './data/ecephys_cache_dir/sessions/adj_mat_ccg_highland_corrected_stationary/'
+keep_edges_above_threshold(directory, threshold)
+directory = './data/ecephys_cache_dir/sessions/adj_mat_ccg_highland_corrected_running/'
+keep_edges_above_threshold(directory, threshold)
+# %%
+# load stationary and running graphs
+area_dict, active_area_dict, mean_speed_df = load_other_data(session_ids)
+cortical_inds = get_cortical_inds(active_area_dict, visual_regions)
+directory = './data/ecephys_cache_dir/sessions/spiking_sequence/'
+path = directory.replace('spiking_sequence', 'adj_mat_ccg_highland_corrected_stationary')
+G_stationary_dict, offset_stationary_dict, duration_stationary_dict = load_highland_xcorr(path, active_area_dict, weight=True)
+measure = 'ccg'
+G_stationary_dict = remove_gabor(G_stationary_dict)
+######### removed neurons from thalamic region
+G_stationary_dict = remove_thalamic(G_stationary_dict, area_dict, visual_regions)
+path = directory.replace('spiking_sequence', 'adj_mat_ccg_highland_corrected_running')
+G_running_dict, offset_running_dict, duration_running_dict = load_highland_xcorr(path, active_area_dict, weight=True)
+measure = 'ccg'
+G_running_dict = remove_gabor(G_running_dict)
+######### removed neurons from thalamic region
+G_running_dict = remove_thalamic(G_running_dict, area_dict, visual_regions)
+S_stationary_dict = add_sign(G_stationary_dict)
+S_stationary_dict = add_offset(S_stationary_dict, offset_stationary_dict)
+S_stationary_dict = add_duration(S_stationary_dict, duration_stationary_dict)
+S_stationary_dict = add_delay(S_stationary_dict)
+S_running_dict = add_sign(G_running_dict)
+S_running_dict = add_offset(S_running_dict, offset_running_dict)
+S_running_dict = add_duration(S_running_dict, duration_running_dict)
+S_running_dict = add_delay(S_running_dict)
+G_stationary = G_stationary_dict['719161530']['drifting_gratings']
+G_running = G_running_dict['719161530']['drifting_gratings']
+# %%
+# plot density comparison
+fig, ax = plt.subplots(1, 1, figsize=(4, 4))
+ax.bar(range(2), [nx.density(G_stationary), nx.density(G_running)], width=0.3, color='.2')
+ax.set_xticks([0, 1])
+ax.set_xticklabels(['stationary', 'running'])
+ax.set_ylabel('network density')
+plt.tight_layout()
+plt.savefig('./plots/density_stationary_running.pdf', transparent=True)
+# %%
+# within area/excitatory/clustering coefficient
+stimulus2marker = {'Resting\nstate':'s', 'Flashes':'*', 'Drifting\ngratings':'X', 'Static\ngratings':'P', 'Natural\nscenes':r'$\clubsuit$', 'Natural\nmovies':'>'}
+marker_size_dict = {'v':10, '*':22, 'P':13, 'X':13, 'o':11, 's':9.5, 'D':9, 'p':12, '>':10, r'$\clubsuit$':20}
+scatter_size_dict = {'v':10, '*':17, 'P':13, 'X':13, 'o':11, 's':10, 'D':9, 'p':13, '>':12, r'$\clubsuit$':16}
+error_size_dict = {'v':10, '*':24, 'P':16, 'X':16, 'o':11, 's':9., 'D':9, 'p':12, '>':13, r'$\clubsuit$':22}
+
+def plot_bar_whole_stationary_running(G_whole, G_stationary, G_running, area_dict, regions):
+  df = pd.DataFrame()
+  region_connection = np.zeros((len(regions), len(regions)))
+  metric_names = ['density', 'ratio of intra-region connections', 'ratio of excitatory connections', 'cluster']
+  intra_data, density_data, ex_data, in_data, cluster_data = [], [], [], [], []
+  for G in [G_whole, G_stationary, G_running]:
+    nodes = list(G.nodes())
+    node_area = {key: area_dict['719161530'][key] for key in nodes}
+    A = nx.to_numpy_array(G)
+    A[A.nonzero()] = 1
+    for region_ind_i, region_i in enumerate(regions):
+      for region_ind_j, region_j in enumerate(regions):
+        region_indices_i = np.array([k for k, v in node_area.items() if v==region_i])
+        region_indices_j = np.array([k for k, v in node_area.items() if v==region_j])
+        region_indices_i = np.array([nodes.index(i) for i in list(set(region_indices_i) & set(nodes))]) # some nodes not in cc are removed 
+        region_indices_j = np.array([nodes.index(i) for i in list(set(region_indices_j) & set(nodes))])
+        if len(region_indices_i) and len(region_indices_j):
+          region_connection[region_ind_i, region_ind_j] = np.sum(A[region_indices_i[:, None], region_indices_j])
+          assert np.sum(A[region_indices_i[:, None], region_indices_j]) == len(A[region_indices_i[:, None], region_indices_j].nonzero()[0])
+    diag_indx = np.eye(len(regions),dtype=bool)
+    intra_data.append(np.sum(region_connection[diag_indx])/np.sum(region_connection))
+    # inter_data.append(np.sum(region_connection[~diag_indx])/np.sum(region_connection))
+    density_data.append(nx.density(G))
+    signs = list(nx.get_edge_attributes(G, "sign").values())
+    ex_data.append(signs.count(1) / len(signs))
+    in_data.append(signs.count(-1) / len(signs))
+    cluster_data.append(calculate_directed_metric(G, 'clustering'))
+  df = pd.concat([df, pd.DataFrame(np.concatenate((np.repeat(np.array(metric_names), 3)[:,None], np.array(density_data + intra_data + ex_data + cluster_data)[:,None], np.array(['whole', 'stationary', 'running'] * 4)[:,None]), 1), columns=['metric', 'data', 'behavior type'])], ignore_index=True)
+  df['data'] = pd.to_numeric(df['data'])
+  fig, axes = plt.subplots(2, 2, figsize=(10, 8))
+  for i, j in [(f,s) for f in range(axes.shape[0]) for s in range(axes.shape[1])]:
+    m_ind = i * axes.shape[1] + j
+    ax = axes[i, j]
+    x = ['whole', 'stationary', 'running']
+    y = df[df['metric']==metric_names[m_ind]]['data'].values
+    ax.bar(x, y)
+    ax.set_ylabel(metric_names[m_ind], fontsize=15)
+    ax.xaxis.set_tick_params(labelsize=15)
+    ax.yaxis.set_tick_params(labelsize=15)
+    for axis in ['bottom', 'left']:
+      ax.spines[axis].set_linewidth(1.5)
+      ax.spines[axis].set_color('0.2')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.tick_params(width=1.5)
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('./plots/bar_whole_stationary_running.pdf', transparent=True)
+
+S_whole = S_ccg_dict['719161530']['drifting_gratings']
+S_stationary = S_stationary_dict['719161530']['drifting_gratings']
+S_running = S_running_dict['719161530']['drifting_gratings']
+plot_bar_whole_stationary_running(S_whole, S_stationary, S_running, area_dict, visual_regions)
+# %%
+######################## signed motif detection
+with open('./files/intensity_dict.pkl', 'rb') as f:
+  intensity_dict = pickle.load(f)
+with open('./files/coherence_dict.pkl', 'rb') as f:
+  coherence_dict = pickle.load(f)
+with open('./files/sunibi_baseline_intensity_dict.pkl', 'rb') as f:
+  sunibi_baseline_intensity_dict = pickle.load(f)
+with open('./files/sunibi_baseline_coherence_dict.pkl', 'rb') as f:
+  sunibi_baseline_coherence_dict = pickle.load(f)
+with open('./files/intensity_stationary_dict.pkl', 'rb') as f:
+  intensity_stationary_dict = pickle.load(f)
+with open('./files/coherence_stationary_dict.pkl', 'rb') as f:
+  coherence_stationary_dict = pickle.load(f)
+with open('./files/sunibi_baseline_intensity_stationary_dict.pkl', 'rb') as f:
+  sunibi_baseline_intensity_stationary_dict = pickle.load(f)
+with open('./files/sunibi_baseline_coherence_stationary_dict.pkl', 'rb') as f:
+  sunibi_baseline_coherence_stationary_dict = pickle.load(f)
+with open('./files/intensity_running_dict.pkl', 'rb') as f:
+  intensity_running_dict = pickle.load(f)
+with open('./files/coherence_running_dict.pkl', 'rb') as f:
+  coherence_running_dict = pickle.load(f)
+with open('./files/sunibi_baseline_intensity_running_dict.pkl', 'rb') as f:
+  sunibi_baseline_intensity_running_dict = pickle.load(f)
+with open('./files/sunibi_baseline_coherence_running_dict.pkl', 'rb') as f:
+  sunibi_baseline_coherence_running_dict = pickle.load(f)
+################## average intensity across session
+################## first Z score, then average
+num_baseline = 200
+whole_df1, mean_df1, signed_motif_types1 = get_intensity_zscore(intensity_dict, coherence_dict, sunibi_baseline_intensity_dict, sunibi_baseline_coherence_dict, num_baseline=num_baseline) # signed uni bi edge preserved
+whole_df2, mean_df2, signed_motif_types2 = get_intensity_zscore(intensity_stationary_dict, coherence_stationary_dict, sunibi_baseline_intensity_stationary_dict, sunibi_baseline_coherence_stationary_dict, num_baseline=num_baseline) # signed uni bi edge preserved
+whole_df3, mean_df3, signed_motif_types3 = get_intensity_zscore(intensity_running_dict, coherence_running_dict, sunibi_baseline_intensity_running_dict, sunibi_baseline_coherence_running_dict, num_baseline=num_baseline) # signed uni bi edge preserved
+whole_df1['signed motif type'] = whole_df1['signed motif type'].str.replace('-', '\N{MINUS SIGN}') # change minus sign to match width of plus
+whole_df2['signed motif type'] = whole_df2['signed motif type'].str.replace('-', '\N{MINUS SIGN}') # change minus sign to match width of plus
+whole_df3['signed motif type'] = whole_df3['signed motif type'].str.replace('-', '\N{MINUS SIGN}') # change minus sign to match width of plus
+signed_motif_types1 = [mt.replace('-', '\N{MINUS SIGN}') for mt in signed_motif_types1]
+signed_motif_types2 = [mt.replace('-', '\N{MINUS SIGN}') for mt in signed_motif_types2]
+signed_motif_types3 = [mt.replace('-', '\N{MINUS SIGN}') for mt in signed_motif_types3]
+#%%
+def add_missing_motif_type(df, mtype, signed_motif_types):
+  if len(mtype) < len(signed_motif_types):
+    mtype2add = [t for t in signed_motif_types if t not in mtype]
+    for mt in mtype2add:
+      mtype.append(mt)
+      for session_id in session_ids:
+        for stimulus_name in stimulus_names:
+          df = pd.concat([df, pd.DataFrame([[mt, session_id, stimulus_name] + [0] * (df.shape[1]-3)], columns=df.columns)], ignore_index=True)
+    df['intensity z score'] = pd.to_numeric(df['intensity z score'])
+  return df, mtype
+
+signed_motif_types = np.unique(signed_motif_types1+signed_motif_types2+signed_motif_types3).tolist()
+whole_df1, signed_motif_types1 = add_missing_motif_type(whole_df1, signed_motif_types1, signed_motif_types)
+whole_df2, signed_motif_types2 = add_missing_motif_type(whole_df2, signed_motif_types2, signed_motif_types)
+whole_df3, signed_motif_types3 = add_missing_motif_type(whole_df3, signed_motif_types3, signed_motif_types)
+TRIAD_NAMES = ('003', '012', '102', '021D', '021U', '021C', '111D', '111U', '030T', '030C', '201', '120D', '120U', '120C', '210', '300')
+# %%
+def plot_zscore_allmotif_lollipop_whole_stationary_running(df_whole, df_stationary, df_running):
+  # stimulus_order = [s for s in combined_stimulus_names if df.stimulus.str.contains(s).sum()]
+  fig, axes = plt.subplots(3,1, sharex=True, sharey=True, figsize=(50, 3*3))
+  all_smotif_types = set(list(df_whole['signed motif type'].values) + list(df_stationary['signed motif type'].values) + list(df_running['signed motif type'].values))
+  sorted_types = [sorted([smotif for smotif in all_smotif_types if mt in smotif]) for mt in TRIAD_NAMES]
+  sorted_types = [item for sublist in sorted_types for item in sublist]
+  motif_types = TRIAD_NAMES[3:]
+  motif_loc = [np.mean([i for i in range(len(sorted_types)) if mt in sorted_types[i]]) for mt in motif_types]
+  # palette = [plt.cm.tab20(i) for i in range(13)]
+  palette = [[plt.cm.tab20b(i) for i in range(20)][i] for i in [0,2,3,4,6,8,10,12,16,18,19]] + [[plt.cm.tab20c(i) for i in range(20)][i] for i in [4,16]]
+  for s_ind, df in enumerate([df_whole, df_stationary, df_running]):
+    data = df
+    data = data.groupby('signed motif type').mean()
+    ax = axes[s_ind] # spontaneous in the bottom
+    # ax.set_title(combined_stimulus_names[s_ind].replace('\n', ' '), fontsize=35, rotation=0)
+    for t, y in zip(sorted_types, data.loc[sorted_types, "intensity z score"]):
+      color = palette[motif_types.index(t.replace('+', '').replace('\N{MINUS SIGN}', ''))]
+      ax.plot([t,t], [0,y], color=color, marker="o", linewidth=7, markersize=20, markevery=(1,2))
+    ax.set_xlim(-.5,len(sorted_types)+.5)
+    ax.set_xticks([])
+    # ax.set_xticks(motif_loc)
+    # ax.set_xticklabels(labels=motif_types)
+    # ax.xaxis.set_tick_params(labelsize=35, rotation=90)
+    ax.yaxis.set_tick_params(labelsize=45)
+    for axis in ['bottom', 'left']:
+      ax.spines[axis].set_linewidth(4.5)
+      ax.spines[axis].set_color('k')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.tick_params(width=4.5)
+    ax.xaxis.set_tick_params(length=0)
+    ax.set_ylabel('')
+    # if model_names.index(model_name) <= 1:
+    #   ax.set_yscale('symlog')
+    # else:
+    ax.set_ylim(-13, 21)
+  plt.tight_layout()
+  figname = './plots/zscore_all_motifs_lollipop_whole_stationary_running.pdf'
+  plt.savefig(figname, transparent=True)
+  # plt.show()
+
+df_whole = whole_df1[(whole_df1['session']=='719161530')&(whole_df1['stimulus']=='drifting_gratings')]
+df_stationary = whole_df2
+df_running = whole_df3
+plot_zscore_allmotif_lollipop_whole_stationary_running(df_whole, df_stationary, df_running)
+# %%
